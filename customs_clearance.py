50e6dc0964baea94d5562420d2bd3c13859b6706
bekashvelidze <78883386+bekashvelidze@users.noreply.github.com>
2021-02-11 11:06:06 +0400

    Update customs_clearance.py
    added some comments

--- customs_clearance.py
+++ customs_clearance.py
@@ -7,12 +7,17 @@ def calculate():
     current_date = date.today()
     current_year = current_date.year
     print('Here you can calculate car import and Customs clearance fees.')
+    # receive input data
     capacity = float(input('Enter car engine capacity (e.g. 2500): '))
     year = int(input('Enter car production year: '))
     transit_days = int(input('Enter Transit days (1 - 60): '))
+    # calculate car age
     age = current_year - year
+    # calculate import rate according to official calculation formula
     import_rate = (capacity * 0.05) + (capacity * age * 0.0025)
+    # initial rate value
     rate = 0
+# check excise rate with car production year
     if age > 14:
         rate = 2.4
     elif age == 14:
@@ -37,7 +42,7 @@ def calculate():
         rate = 1.4
     elif age == 2 or age == 1 or age == 0:
         rate = 1.5
-
+# calculate excise and final clearance price
     excise = capacity * rate
     final_price = import_rate + excise + 150 + 20 + 30 + 72 + transit_days
     print('Final price for your car import and Customs clearance is:', math.ceil(final_price))
@@ -45,6 +50,7 @@ def calculate():
 
 calculate()
 time.sleep(0.5)
+# reset to make new calculation
 reset = input("Enter 'yes' to make new calculation or 'no' to exit: - ")
 if reset == 'yes':
     calculate()
